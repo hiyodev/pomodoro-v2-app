79f6d00879dcbe367a1d618005063f87a147d327
Nobu <hiyodev@gmail.com>
2023-07-11 19:14:03 +0800

    Updated AddBar Buttons

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,6 +1,19 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  MenuItem,
+  Menu,
+  IconButton,
+} from "@mui/material";
 import ThemeToggleSwitch from "../Buttons/ThemeToggleSwitch";
 import AddCardIcon from "@mui/icons-material/AddCard";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import NotificationsOffIcon from "@mui/icons-material/NotificationsOff";
+
+import { useState } from "react";
 
 interface Props {
   darkMode: boolean;
@@ -8,16 +21,66 @@ interface Props {
 }
 
 export const TopBar = ({ darkMode, setDarkMode }: Props): JSX.Element => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h5" sx={{ flex: 1 }}>
           Hiyo Pomodoro V2
         </Typography>
-        <Button sx={{ maxWidth: 45, minWidth: 0 }}>
-          <AddCardIcon />
-        </Button>
-        <ThemeToggleSwitch darkMode={darkMode} setDarkMode={setDarkMode} />
+        <div>
+          <IconButton
+            size="large"
+            aria-label="more settings"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            sx={{ textAlign: "right" }}
+          >
+            <MenuItem>
+              <ThemeToggleSwitch
+                darkMode={darkMode}
+                setDarkMode={setDarkMode}
+              />
+              <Typography sx={{ marginLeft: -2 }}>Toggle Theme</Typography>
+            </MenuItem>
+            <MenuItem>
+              <AddCardIcon color="inherit" />
+              <Typography sx={{ marginLeft: 2 }}>Add Timer Card</Typography>
+            </MenuItem>
+            <MenuItem>
+              <NotificationsActiveIcon color="inherit" />
+              <Typography sx={{ marginLeft: 2 }}>Notifications On</Typography>
+            </MenuItem>
+          </Menu>
+        </div>
       </Toolbar>
     </AppBar>
   );
